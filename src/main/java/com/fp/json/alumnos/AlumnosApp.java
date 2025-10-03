0a2b6675eb0adc33c2b61ab834913e56154f3a66
Ruben Leis <rubenleisfp@gmail.com>
2025-10-03 10:13:22 +0200

    Primeros pasos

--- src/main/java/com/fp/json/alumnos/AlumnosApp.java
+++ src/main/java/com/fp/json/alumnos/AlumnosApp.java
@@ -4,43 +4,58 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class AlumnosApp {
     public static void main(String[] args) {
         try {
-            // Crear ObjectMapper
-            ObjectMapper mapper = new ObjectMapper();
-
-            // Leer JSON a List<Alumno>
-            List<Alumno> alumnos = mapper.readValue(
-                    new File("src/main/resources/json/alumnos/alumnos_asignaturas.json"),
-                    new TypeReference<List<Alumno>>() {}
-            );
-
-            // Mostrar alumnos
-            System.out.println("Lista de alumnos:");
-            for (Alumno alumno : alumnos) {
-                System.out.println(alumno);
-            }
-
-            // Calcular media de calificaciones
-            double suma = 0;
-            int contador = 0;
-
-            for (Alumno alumno : alumnos) {
-                for (Asignatura asignatura : alumno.getAsignaturas()) {
-                    suma += asignatura.getCalificacion();
-                    contador++;
-                }
-            }
-
-            double media = (contador > 0) ? (suma / contador) : 0;
-
-            System.out.println("\nCalificación media de todos los alumnos: " + media);
-
+            List<Alumno> alumnos = getAlumnos();
+            mostrarAlumnos(alumnos);
+            mostrarNotaMedia(alumnos);
         } catch (Exception e) {
-            e.printStackTrace();
+           System.err.println(e.getMessage());
         }
     }
+
+    private static List<Alumno> getAlumnos() throws IOException {
+        // Crear ObjectMapper
+        ObjectMapper mapper = new ObjectMapper();
+
+        // Leer JSON a List<Alumno>
+        List<Alumno> alumnos = mapper.readValue(
+                new File("src/main/resources/json/alumnos/alumnos_asignaturas.json"),
+                new TypeReference<List<Alumno>>() {}
+        );
+        return alumnos;
+    }
+
+    private static void mostrarAlumnos(List<Alumno> alumnos) {
+        // Mostrar alumnos
+        System.out.println("Lista de alumnos:");
+        for (Alumno alumno : alumnos) {
+            System.out.println(alumno);
+        }
+    }
+
+    private static void mostrarNotaMedia(List<Alumno> alumnos) {
+        // Calcular media de calificaciones
+        double suma = 0;
+        int contador = 0;
+
+        for (Alumno alumno : alumnos) {
+            for (Asignatura asignatura : alumno.getAsignaturas()) {
+                suma += asignatura.getCalificacion();
+                contador++;
+            }
+        }
+
+        double media = (contador > 0) ? (suma / contador) : 0;
+
+        System.out.println("\nCalificación media de todos los alumnos: " + media);
+    }
+
+
+
+
 }
