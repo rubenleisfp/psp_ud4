513aef9183df2066bef72cccf0f93c4ef274b0d2
Ruben Leis <rubenleisfp@gmail.com>
2025-10-05 21:53:32 +0200

    Limpiando para el alumnos

--- src/main/java/com/fp/json/alumnos/AlumnosApp.java
+++ src/main/java/com/fp/json/alumnos/AlumnosApp.java
@@ -19,43 +19,18 @@ public class AlumnosApp {
     }
 
     private static List<Alumno> getAlumnos() throws IOException {
-        // Crear ObjectMapper
-        ObjectMapper mapper = new ObjectMapper();
+        throw new UnsupportedOperationException("A implementar por el alumno");
 
-        // Leer JSON a List<Alumno>
-        List<Alumno> alumnos = mapper.readValue(
-                new File("src/main/resources/json/alumnos/alumnos_asignaturas.json"),
-                new TypeReference<List<Alumno>>() {}
-        );
-        return alumnos;
     }
 
     private static void mostrarAlumnos(List<Alumno> alumnos) {
-        // Mostrar alumnos
-        System.out.println("Lista de alumnos:");
-        for (Alumno alumno : alumnos) {
-            System.out.println(alumno);
-        }
+        throw new UnsupportedOperationException("A implementar por el alumno");
+
     }
 
     private static void mostrarNotaMedia(List<Alumno> alumnos) {
-        // Calcular media de calificaciones
-        double suma = 0;
-        int contador = 0;
-
-        for (Alumno alumno : alumnos) {
-            for (Asignatura asignatura : alumno.getAsignaturas()) {
-                suma += asignatura.getCalificacion();
-                contador++;
-            }
-        }
-
-        double media = (contador > 0) ? (suma / contador) : 0;
-
-        System.out.println("\nCalificación media de todos los alumnos: " + media);
+        throw new UnsupportedOperationException("A implementar por el alumno");
     }
-
-
 
 
 }
--- src/main/java/com/fp/json/ejemplos/MkyongExamples.java
+++ src/main/java/com/fp/json/ejemplos/MkyongExamples.java
@@ -19,76 +19,21 @@ public class MkyongExamples {
      * Escribir un objeto Java en un String
      */
     private static void convertObjectToJson () {
-        Person person = new Person("mkyong", 42);
-
-        ObjectMapper om = new ObjectMapper();
-
-        try {
-
-            // covert Java object to JSON strings
-            String json = om.writeValueAsString(person);
-
-            // output: {"name":"mkyong","age":42}
-            System.out.println(json);
-
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-
-
+        throw new UnsupportedOperationException("A implementar por el alumno");
     }
 
     /**
      * Leer un String JSON y convertirlo en un objeto Java
      */
     private static void convertJsonToObject() {
-        String json = "{\"name\": \"mkyong\", \"age\": 20}";
-
-        ObjectMapper om = new ObjectMapper();
-
-        try {
-
-            // covert JSON to Java object
-            Person person = om.readValue(json, Person.class);
-
-            // output: Person{name='mkyong', age=20}
-            System.out.println(person);
-
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-
-
+        throw new UnsupportedOperationException("A implementar por el alumno");
     }
 
     private static void writeObjectToJsonFile() {
-        Person person = new Person("mkyong", 42);
-
-        ObjectMapper om = new ObjectMapper();
-
-        try {
-
-            // convert Java object to JSON strings and save it to file `person.json`
-            om.writeValue(new File("person.json"), person);
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
+        throw new UnsupportedOperationException("A implementar por el alumno");
     }
 
     private static void readObjectFromJsonFile() {
-        ObjectMapper om = new ObjectMapper();
-
-        try {
-
-            // read JSON from a file and convert it to Java object
-            Person person = om.readValue(new File("person.json"), Person.class);
-
-            System.out.println(person);
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        throw new UnsupportedOperationException("A implementar por el alumno");
     }
 }
